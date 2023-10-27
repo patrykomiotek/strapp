6e4ad8c0d8c60ab77f63faf0c9ef131fdbb4a9e9
Patryk Omiotek <patryk.omiotek@gmail.com>
2023-10-27 10:38:42 +0200

    feat: mocking tests

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <h1>Start app</h1>
-      <DisplayBooks />
+      {/* <DisplayBooks /> */}
       <AuthProvider>
         <AuthInfo />
       </AuthProvider>
--- src/components/DisplayBooks.spec.tsx
+++ src/components/DisplayBooks.spec.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { DisplayBooks } from "./DisplayBooks";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 describe("DisplayBooks component", () => {
-  it("should display mocked data", () => {
-    const client = new ApolloClient({
+  it.skip("should display mocked data", () => {
+    const cache = new InMemoryCache();
+
+    const link = new HttpLink({
       uri: "http://localhost:4000",
-      cache: new InMemoryCache(),
+
+      // Use explicit `window.fetch` so that outgoing requests
+      // are captured and deferred until the Service Worker is ready.
+      fetch: (...args) => fetch(...args),
     });
+
+    // Isolate Apollo client so it could be reused
+    // in both application runtime and tests.
+    const client = new ApolloClient({
+      cache,
+      link,
+    });
+
     const { debug } = render(
       <ApolloProvider client={client}>
         <DisplayBooks />
